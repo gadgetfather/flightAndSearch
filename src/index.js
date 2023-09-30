09449d735a986fa21d54d880ceaa41aec4ae9430
Akshay Kamble <kamblea635@gmail.com>
2023-09-30 20:35:22 +0530

    create config file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const { PORT } = require("./config/serverConfig");
 const setupAndStartServer = () => {
   //create the express app
   const app = express();
   //configure dotenv
-  dotenv.config();
+
   //configure the express app
-  const port = process.env.PORT;
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
   });
 };
 
